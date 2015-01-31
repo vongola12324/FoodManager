5ad835e0c49cc8f40ad42100680ec70052391371
Chan, Danny <danny50610@yahoo.com.tw>
2015-02-01 07:21:15 +0800

    add some data

--- app/src/main/java/creepomb/foodmanager/db/DBHelper.java
+++ app/src/main/java/creepomb/foodmanager/db/DBHelper.java
@@ -35,8 +35,17 @@ public class DBHelper extends SQLiteOpenHelper {
                 "(" + DBStorageLocationItemsProcess.KEY_ID + "," + DBStorageLocationItemsProcess.ICONINDEX_COLUMN + "," + DBStorageLocationItemsProcess.NAME_COLUMN + ")" +
                 "VALUES (2, 2, '零食櫃');");
 
-        db.execSQL(DBFoodItemsProcess.getSQL_INSERT(new FoodItem("伊賀", 1, "隻", 1, new GregorianCalendar(2075, 7, 17).getTimeInMillis(), 0)));
-
+        db.execSQL(DBFoodItemsProcess.getSQL_INSERT(new FoodItem("伊賀", 1, "隻", 1, new GregorianCalendar(2075, 7, 17).getTimeInMillis(), 2)));
+        db.execSQL(DBFoodItemsProcess.getSQL_INSERT(new FoodItem("香蕉", 5, "串", 1, new GregorianCalendar(2015, 2, 7).getTimeInMillis(), 1)));
+        db.execSQL(DBFoodItemsProcess.getSQL_INSERT(new FoodItem("蘋果", 1, "隻", 1, new GregorianCalendar(2015, 2, 5).getTimeInMillis(), 0)));
+        db.execSQL(DBFoodItemsProcess.getSQL_INSERT(new FoodItem("泡麵", 10, "箱", 2, new GregorianCalendar(2016, 2, 1).getTimeInMillis(), 0)));
+        /*db.execSQL(DBFoodItemsProcess.getSQL_INSERT(new FoodItem("", 1, "隻", 1, new GregorianCalendar(2075, 7, 17).getTimeInMillis(), 0)));
+        db.execSQL(DBFoodItemsProcess.getSQL_INSERT(new FoodItem("", 1, "隻", 1, new GregorianCalendar(2075, 7, 17).getTimeInMillis(), 0)));
+        db.execSQL(DBFoodItemsProcess.getSQL_INSERT(new FoodItem("", 1, "隻", 1, new GregorianCalendar(2075, 7, 17).getTimeInMillis(), 0)));
+        db.execSQL(DBFoodItemsProcess.getSQL_INSERT(new FoodItem("", 1, "隻", 1, new GregorianCalendar(2075, 7, 17).getTimeInMillis(), 0)));
+        db.execSQL(DBFoodItemsProcess.getSQL_INSERT(new FoodItem("", 1, "隻", 1, new GregorianCalendar(2075, 7, 17).getTimeInMillis(), 0)));
+        db.execSQL(DBFoodItemsProcess.getSQL_INSERT(new FoodItem("", 1, "隻", 1, new GregorianCalendar(2075, 7, 17).getTimeInMillis(), 0)));
+*/
     }
 
     @Override
